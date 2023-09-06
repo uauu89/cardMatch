4da7688231da22c2ac10b06def910caa766ff72c
uauu89 <uauu89@gmail.com>
2023-09-06 22:28:10 +0900

    코드정리, ai 알고리즘 작성중

--- src/test.js
+++ src/test.js
@@ -1,7 +1,121 @@
-let cardArray = [1, 2, 3, 4, 5, 6, 7, 8];
+let cardArray, openCard, cardArrayCopy;
+let userDelay = false;
 
-let openCard = "x".repeat(cardArray.length*2).split("");
 
+document.querySelector("#newGame").addEventListener("click", e=>{
+    init();
+})
+
+function init(){
+    setCardArray();
+    cardShuffleSimple(cardArrayCopy);
+    insertCard(cardArrayCopy);
+    
+    cardCheck();
+
+
+    turn_user();
+
+}
+
+    function setCardArray(){
+        cardArray = [];
+        let i = 0;
+        while(i < document.querySelector("#inputNum").value){
+            cardArray.push(i+1)
+            i++;
+        }
+
+        cardArrayCopy = [...cardArray, ...cardArray];
+        openCard = "x".repeat(cardArray.length*2).split("");
+    }
+
+    function cardShuffleSimple(array){
+        array.sort(()=>Math.random() - 0.5);
+    }
+
+    function insertCard(array){
+        let dom_insertHTML = "";
+        array.forEach((i, idx)=>{
+            dom_insertHTML += `
+            <div class="card_wrap" data-value=${i} data-index=${idx}>
+                <div class="card card_front">${i}</div>
+                <div class="card card_back"></div>
+            </div>`;
+        });
+        document.querySelector(".wrap").innerHTML = dom_insertHTML;
+    }
+
+    function cardCheck(){
+        document.querySelectorAll(".card_wrap").forEach((card, idx)=>{
+            setTimeout(() => {
+                card.classList.add("card_open")
+            }, idx*200);
+
+            setTimeout(() => {
+                card.classList.remove("card_open")
+            }, idx*100+(cardArray.length*2*200)+3000);
+        })
+        setTimeout(()=>{userDelay = true}, (cardArray.length*2*300+3000));
+    }
+
+
+function gamePlay(){
+    turn_user();
+}
+
+    function turn_user(){
+        let userSelectedCard = [];
+        document.querySelectorAll(".card_wrap").forEach((card, idx) => {
+
+            card.addEventListener("click", e=>{
+
+                if(userDelay && !card.classList.contains("card_open") && !card.classList.contains("correct")){
+                    userDelay = false;
+
+                    e.currentTarget.classList.add("card_open");
+                    openCard[idx] = e.currentTarget.dataset.value;
+
+                    userSelectedCard.push(e.currentTarget.dataset.value);
+
+                    setTimeout(()=>{
+                        userDelay = true;
+
+                        if(userSelectedCard.length === 2){
+                            if(userSelectedCard[0] === userSelectedCard[1]){
+                                console.log("match success")
+                                for(let i of document.querySelectorAll(".card_open")){
+                                    i.setAttribute("class", "card_wrap correct user");
+                                }
+                            }else{
+                                for(let i of document.querySelectorAll(".card_open")){
+                                    i.classList.remove("card_open");
+                                }
+                            }
+                            userSelectedCard = [];
+                        }
+                    }, 500);
+                }
+            })
+        });
+    }
+    function turn_com(){
+        // const getElCount = arr => arr.reduce((ac, v) => (
+        //     { ...ac, [v]: (ac[v] || 0) + 1 }), {});
+        
+        // const array = ['A', 'B', 'C', 'D', 'A', 'A', 'C', 'E', 'D', 'E', 'A'];
+        // const test = getElCount(array);
+        // console.log(test);
+    }
+
+
+    function checkPair(){
+        let checkArray = [];
+
+        // document.querySelectorAll(".card_wrap:not(.correct)")
+    }
+    
+/*
 function cardShuffle(array){
     let cardArrayCopy = [...array, ...array];
     let cardArrayCount = cardArrayCopy.length;
@@ -13,52 +127,7 @@ function cardShuffle(array){
     }
     return cardArrayShuffle;
 }
-
-
-function cardShuffleSimple(array){
-    array.sort(()=>Math.random() - 0.5);
-}
-
-let cardArrayCopy = [...cardArray, ...cardArray];
-
-cardShuffleSimple(cardArrayCopy);
-
-
-// -------loop test
-
-
-let loopArray = [...cardArray];
-function loopShuffle(times){
-    let looptimes = 0;
-    while(looptimes < times){
-        // console.log("cardArrayCopy : ", loopArray);
-        // console.log("shuffle : ", cardShuffle(loopArray));
-        console.log(cardShuffle(loopArray));
-        looptimes++;
-    }
-}
-function loopShuffleSimple(times){
-    let looptimes = 0;
-    while(looptimes < times){
-        let copyLoopArray = [...loopArray, ...loopArray,];
-        copyLoopArray.sort(()=>Math.random() - 0.5);
-        console.log(copyLoopArray);
-        looptimes++;
-    }
-}
-
-// -------loop test
-
-// offsetwidth
-
-let dom_insertHTML = "";
-
-for(let i of cardArrayCopy){
-    dom_insertHTML += `<div class="card_wrap" data-value=${i}><div class="card card_front">${i}</div><div class="card card_back"></div></div>`;
-}
-document.querySelector(".wrap").innerHTML = dom_insertHTML;
-
-
+*/
 
 
 let cardWrap = document.querySelectorAll(".card_wrap"),
@@ -72,30 +141,21 @@ let checkCardNum = 2,
 notice.innerText = score;
 
 
-function singleStart(){
-    for(let _time = 0; _time < cardWrap.length; _time++){
-        console.log(_time);
-        setTimeout(()=>{
-            cardWrap[_time].classList.add("card_click");
-        }, 200 * (_time + 1) );
-        setTimeout(()=>{
-            cardWrap[_time].classList.remove("card_click");
-        }, cardWrap.length * 200 + 2000 + 50 * _time)
-    }
-    checkCardNum = 0;
-}
-
-singleStart();
-
 
 // 인덱스 번호 구하기 위해 forEach로 바꾸기
 
+
+
+
+
+
+
+/*
 for(let i of cardWrap){
     i.addEventListener("click", (e)=>{
-        console.log(e.currentTarget.dataset.value);
-        
-        if(!e.currentTarget.classList.contains("card_click") && !e.currentTarget.classList.contains("correct") && checkCardNum < 2){
-            i.classList.add("card_click");
+        // console.log(e.currentTarget.dataset.value);
+        if(!e.currentTarget.classList.contains("card_open") && !e.currentTarget.classList.contains("correct") && checkCardNum < 2){
+            i.classList.add("card_open");
             checkCardNum++;
             // console.log(e.currentTarget.dataset.value)
             selectedNum.push(e.currentTarget.dataset.value);
@@ -103,13 +163,13 @@ for(let i of cardWrap){
             if(checkCardNum == 2){
                 setTimeout(()=>{
                     if(selectedNum[0] == selectedNum[1]){
-                        for(let j of document.querySelectorAll(".card_click")){
+                        for(let j of document.querySelectorAll(".card_open")){
                             j.setAttribute("class", "card_wrap correct");
                         }
                         score += 100
                     }else{
                         for(let j of cardWrap){
-                            j.classList.remove("card_click");
+                            j.classList.remove("card_open");
                         }
                         score -= 10
                     }
@@ -121,3 +181,4 @@ for(let i of cardWrap){
         }
     })
 }
+*/
