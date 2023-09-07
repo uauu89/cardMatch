7041000110de7d37b1f47782df31bf343e0cd658
uauu89 <uauu89@gmail.com>
2023-09-07 15:08:45 +0900

    openCard 구조 수정중

--- src/test.js
+++ src/test.js
@@ -1,6 +1,25 @@
 let cardArray, openCard, cardArrayCopy;
 let userDelay = false;
 
+let whosTurn = 0;
+
+/*
+모든 인덱스값을 가진 배열에서 맞춘 카드의 인덱스 값을 replice로 제거를 하는게 더 낫지 않을까
+> 오픈되었던 카드인지 아닌지 구별 불가능
+> 현재 오픈된 카드인지 아닌지 구별 불가능
+
+
+함수화 할 것들
+openCard에 인덱스번호 추가하는 것만 (타이머와 분리)
+짝을 맞췄을 경우 correct 판정하기 (변수로 유저 = 0 /컴퓨터 = 1 구분해서 누가 맞췄는지 구분하도록 작성?)
+
+현재 aiTest 문제점
+
+openCard 배열에 각 인덱스 위치에 카드번호를 저장
+그런데 aiArray에서 x를 제거하고 value값을 인덱스번호로 이용해서 오류
+
+
+*/
 
 document.querySelector("#newGame").addEventListener("click", e=>{
     init();
@@ -60,6 +79,27 @@ function init(){
     }
 
 
+
+function pushOpenCard(idx, value){
+    openCard[idx] = value;
+    document.querySelectorAll(".card_wrap")[idx].classList.add("card_open");
+}
+
+function checkMatching(arr, who){
+    let winner = "";
+    if(arr[0] === arr[1]){
+        who === 0? winner = "card_wrap correct user": winner = "card_wrap correct com";
+        for(let i of document.querySelectorAll(".card_open")){
+            i.setAttribute("class", winner);
+        }
+    }else{
+        for(let i of document.querySelectorAll(".card_open")){
+            i.classList.remove("card_open");
+        }
+    }
+}
+
+
 function gamePlay(){
     turn_user();
 }
@@ -73,8 +113,10 @@ function gamePlay(){
                 if(userDelay && !card.classList.contains("card_open") && !card.classList.contains("correct")){
                     userDelay = false;
 
-                    e.currentTarget.classList.add("card_open");
-                    openCard[idx] = e.currentTarget.dataset.value;
+                    // e.currentTarget.classList.add("card_open");
+                    // openCard[idx] = e.currentTarget.dataset.value;
+
+                    pushOpenCard(idx, e.currentTarget.dataset.value);
 
                     userSelectedCard.push(e.currentTarget.dataset.value);
 
@@ -82,16 +124,18 @@ function gamePlay(){
                         userDelay = true;
 
                         if(userSelectedCard.length === 2){
-                            if(userSelectedCard[0] === userSelectedCard[1]){
-                                console.log("match success")
-                                for(let i of document.querySelectorAll(".card_open")){
-                                    i.setAttribute("class", "card_wrap correct user");
-                                }
-                            }else{
-                                for(let i of document.querySelectorAll(".card_open")){
-                                    i.classList.remove("card_open");
-                                }
-                            }
+                            checkMatching(userSelectedCard, whosTurn);
+
+                            // if(userSelectedCard[0] === userSelectedCard[1]){
+                            //     console.log("match success")
+                            //     for(let i of document.querySelectorAll(".card_open")){
+                            //         i.setAttribute("class", "card_wrap correct user");
+                            //     }
+                            // }else{
+                            //     for(let i of document.querySelectorAll(".card_open")){
+                            //         i.classList.remove("card_open");
+                            //     }
+                            // }
                             userSelectedCard = [];
                         }
                     }, 500);
@@ -106,8 +150,51 @@ function gamePlay(){
         // const array = ['A', 'B', 'C', 'D', 'A', 'A', 'C', 'E', 'D', 'E', 'A'];
         // const test = getElCount(array);
         // console.log(test);
+
+        // console.log(document.querySelectorAll(".card_wrap:not(.correct)").length);
+        // let aiArray = [...document.querySelectorAll(".card_wrap:not(.correct)")].sort(()=>Math.random() - 0.5);
+        // let aiArray = openCard.filter((num)=>{
+        //     return num !== "x";
+        // });
+
+        let aiArray = [];
+
+        openCard.forEach((i, idx)=>{
+            if(i !== "x"){
+                aiArray.push(idx);
+            }
+        })
+        
+        if(aiArray.length){
+            aiArray.sort(()=>Math.random() - 0.5);
+            console.log("origin array : ", aiArray);
+            let firstIndex = Number(aiArray.shift());
+            console.log("firstIndex : ", firstIndex);
+            console.log("remove firstIndex : ", aiArray);
+            document.querySelectorAll(".card_wrap:not(.correct)")[firstIndex].classList.add("card_open");
+            setTimeout(()=>{
+                let secondIndex = Number(aiArray.shift());
+                console.log("secondIndex : ", secondIndex);
+                document.querySelectorAll(".card_wrap:not(.correct)")[secondIndex].classList.add("card_open");
+                console.log("remove secondIndex : ", aiArray);
+            }, 300)
+
+            setTimeout(()=>{
+                for(let i of document.querySelectorAll(".card_open")){
+                    i.classList.remove("card_open");
+                }
+            }, 1000)
+
+        }else{
+            console.log("aiArray false");
+        }
+        
     }
 
+
+document.querySelector("#aiTest").addEventListener("click", e=>{
+    turn_com();
+})
 
     function checkPair(){
         let checkArray = [];
