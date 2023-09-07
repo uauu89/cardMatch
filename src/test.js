2d6d17fef50a50ba2ee3664455741d99ed681d10
uauu89 <uauu89@gmail.com>
2023-09-07 21:12:04 +0900

    동일 작업중 (com 알고리즘>리스트 생성)

--- src/test.js
+++ src/test.js
@@ -30,7 +30,7 @@ function init(){
     cardShuffleSimple(cardArrayCopy);
     insertCard(cardArrayCopy);
     
-    cardCheck();
+    cardCheck(false);
 
 
     turn_user();
@@ -65,23 +65,29 @@ function init(){
         document.querySelector(".wrap").innerHTML = dom_insertHTML;
     }
 
-    function cardCheck(){
-        document.querySelectorAll(".card_wrap").forEach((card, idx)=>{
-            setTimeout(() => {
-                card.classList.add("card_open")
-            }, idx*200);
-
-            setTimeout(() => {
-                card.classList.remove("card_open")
-            }, idx*100+(cardArray.length*2*200)+3000);
-        })
-        setTimeout(()=>{userDelay = true}, (cardArray.length*2*300+3000));
+    function cardCheck(bool){
+        if(bool){
+            document.querySelectorAll(".card_wrap").forEach((card, idx)=>{
+                setTimeout(() => {
+                    card.classList.add("card_open")
+                }, idx*200);
+    
+                setTimeout(() => {
+                    card.classList.remove("card_open")
+                }, idx*100+(cardArray.length*2*200)+3000);
+            })
+            setTimeout(()=>{
+                userDelay = true;
+            }, (cardArray.length*2*300+3000));
+        }else{
+            userDelay = true;
+        }
     }
 
 
 
-function pushOpenCard(idx, value){
-    openCard[idx] = value;
+function pushOpenCard(idx){
+    openCard[idx] = document.querySelectorAll(".card_wrap")[idx].dataset.value;
     document.querySelectorAll(".card_wrap")[idx].classList.add("card_open");
 }
 
@@ -100,172 +106,116 @@ function checkMatching(arr, who){
 }
 
 
-function gamePlay(){
-    turn_user();
-}
+function turn_user(){
+    let userSelectedCard = [];
+    document.querySelectorAll(".card_wrap").forEach((card, idx) => {
 
-    function turn_user(){
-        let userSelectedCard = [];
-        document.querySelectorAll(".card_wrap").forEach((card, idx) => {
+        card.addEventListener("click", e=>{
 
-            card.addEventListener("click", e=>{
+            if(userDelay && !card.classList.contains("card_open") && !card.classList.contains("correct")){
+                userDelay = false;
 
-                if(userDelay && !card.classList.contains("card_open") && !card.classList.contains("correct")){
-                    userDelay = false;
+                // e.currentTarget.classList.add("card_open");
+                // openCard[idx] = e.currentTarget.dataset.value;
 
-                    // e.currentTarget.classList.add("card_open");
-                    // openCard[idx] = e.currentTarget.dataset.value;
+                pushOpenCard(idx);
 
-                    pushOpenCard(idx, e.currentTarget.dataset.value);
+                userSelectedCard.push(e.currentTarget.dataset.value);
 
-                    userSelectedCard.push(e.currentTarget.dataset.value);
+                setTimeout(()=>{
+                    userDelay = true;
 
-                    setTimeout(()=>{
-                        userDelay = true;
+                    if(userSelectedCard.length === 2){
+                        checkMatching(userSelectedCard, whosTurn);
 
-                        if(userSelectedCard.length === 2){
-                            checkMatching(userSelectedCard, whosTurn);
-
-                            // if(userSelectedCard[0] === userSelectedCard[1]){
-                            //     console.log("match success")
-                            //     for(let i of document.querySelectorAll(".card_open")){
-                            //         i.setAttribute("class", "card_wrap correct user");
-                            //     }
-                            // }else{
-                            //     for(let i of document.querySelectorAll(".card_open")){
-                            //         i.classList.remove("card_open");
-                            //     }
-                            // }
-                            userSelectedCard = [];
-                        }
-                    }, 500);
-                }
-            })
-        });
-    }
-    function turn_com(){
-        // const getElCount = arr => arr.reduce((ac, v) => (
-        //     { ...ac, [v]: (ac[v] || 0) + 1 }), {});
-        
-        // const array = ['A', 'B', 'C', 'D', 'A', 'A', 'C', 'E', 'D', 'E', 'A'];
-        // const test = getElCount(array);
-        // console.log(test);
-
-        // console.log(document.querySelectorAll(".card_wrap:not(.correct)").length);
-        // let aiArray = [...document.querySelectorAll(".card_wrap:not(.correct)")].sort(()=>Math.random() - 0.5);
-        // let aiArray = openCard.filter((num)=>{
-        //     return num !== "x";
-        // });
-
-        let aiArray = [];
-
-        openCard.forEach((i, idx)=>{
-            if(i !== "x"){
-                aiArray.push(idx);
+                        // if(userSelectedCard[0] === userSelectedCard[1]){
+                        //     console.log("match success")
+                        //     for(let i of document.querySelectorAll(".card_open")){
+                        //         i.setAttribute("class", "card_wrap correct user");
+                        //     }
+                        // }else{
+                        //     for(let i of document.querySelectorAll(".card_open")){
+                        //         i.classList.remove("card_open");
+                        //     }
+                        // }
+                        userSelectedCard = [];
+                    }
+                }, 500);
             }
         })
-        
-        if(aiArray.length){
-            aiArray.sort(()=>Math.random() - 0.5);
-            console.log("origin array : ", aiArray);
-            let firstIndex = Number(aiArray.shift());
-            console.log("firstIndex : ", firstIndex);
-            console.log("remove firstIndex : ", aiArray);
-            document.querySelectorAll(".card_wrap:not(.correct)")[firstIndex].classList.add("card_open");
-            setTimeout(()=>{
-                let secondIndex = Number(aiArray.shift());
-                console.log("secondIndex : ", secondIndex);
-                document.querySelectorAll(".card_wrap:not(.correct)")[secondIndex].classList.add("card_open");
-                console.log("remove secondIndex : ", aiArray);
-            }, 300)
+    });
+}
+function turn_com(){
+    // const getElCount = arr => arr.reduce((ac, v) => (
+    //     { ...ac, [v]: (ac[v] || 0) + 1 }), {});
+    
+    // const array = ['A', 'B', 'C', 'D', 'A', 'A', 'C', 'E', 'D', 'E', 'A'];
+    // const test = getElCount(array);
+    // console.log(test);
 
-            setTimeout(()=>{
-                for(let i of document.querySelectorAll(".card_open")){
-                    i.classList.remove("card_open");
-                }
-            }, 1000)
+    // console.log(document.querySelectorAll(".card_wrap:not(.correct)").length);
+    // let aiArray = [...document.querySelectorAll(".card_wrap:not(.correct)")].sort(()=>Math.random() - 0.5);
+    // let aiArray = openCard.filter((num)=>{
+    //     return num !== "x";
+    // });
 
-        }else{
-            console.log("aiArray false");
+    let aiArray = [];
+
+    openCard.forEach((i, idx)=>{
+        if(i !== "x"){
+            aiArray.push(idx);
         }
-        
+    })
+    
+    if(aiArray.length){
+        aiArray.sort(()=>Math.random() - 0.5);
+        let firstIndex =aiArray.shift();
+        document.querySelectorAll(".card_wrap:not(.correct)")[firstIndex].classList.add("card_open");
+        setTimeout(()=>{
+            let secondIndex = aiArray.shift();
+            document.querySelectorAll(".card_wrap:not(.correct)")[secondIndex].classList.add("card_open");
+        }, 300)
+
+        setTimeout(()=>{
+            for(let i of document.querySelectorAll(".card_open")){
+                i.classList.remove("card_open");
+            }
+        }, 1000)
+
+    }else{
+        console.log("aiArray false");
     }
+}
+
+function com_createArray(){
+    let openCardMod = [];
+    openCard.forEach((i, idx)=>{
+        if(i !== "x"){
+            openCardMod.push(idx);
+        }
+    })
+    let test_openArray = [...openCardMod].sort(()=>Math.random() - 0.5);
+    if(test_openArray.length){
+        let idx = test_openArray.shift();
+        pushOpenCard(idx);
+    }else{
+        test_openArray = document.querySelectorAll(".card_wrap:not(.correct):not(.card_oepn)");
+        let randomNum = Math.floor(Math.random()*test_openArray.length);
+        pushOpenCard(randomNum);
+    }
+}
+
+
+
+
+
+
+
 
 
 document.querySelector("#aiTest").addEventListener("click", e=>{
     turn_com();
 })
-
-    function checkPair(){
-        let checkArray = [];
-
-        // document.querySelectorAll(".card_wrap:not(.correct)")
-    }
-    
-/*
-function cardShuffle(array){
-    let cardArrayCopy = [...array, ...array];
-    let cardArrayCount = cardArrayCopy.length;
-    let cardArrayShuffle = [];
-
-    for(let i = 0; i < array.length*2; i++){
-        cardArrayShuffle.push(cardArrayCopy.splice([Math.floor(Math.random() * cardArrayCount)], 1)[0]);
-        --cardArrayCount;
-    }
-    return cardArrayShuffle;
-}
-*/
-
-
-let cardWrap = document.querySelectorAll(".card_wrap"),
-    notice = document.querySelector(".notice span")
-    
-
-let checkCardNum = 2,
-    selectedNum = [],
-    score = 0;
-
-notice.innerText = score;
-
-
-
-// 인덱스 번호 구하기 위해 forEach로 바꾸기
-
-
-
-
-
-
-
-/*
-for(let i of cardWrap){
-    i.addEventListener("click", (e)=>{
-        // console.log(e.currentTarget.dataset.value);
-        if(!e.currentTarget.classList.contains("card_open") && !e.currentTarget.classList.contains("correct") && checkCardNum < 2){
-            i.classList.add("card_open");
-            checkCardNum++;
-            // console.log(e.currentTarget.dataset.value)
-            selectedNum.push(e.currentTarget.dataset.value);
-            console.log(selectedNum);
-            if(checkCardNum == 2){
-                setTimeout(()=>{
-                    if(selectedNum[0] == selectedNum[1]){
-                        for(let j of document.querySelectorAll(".card_open")){
-                            j.setAttribute("class", "card_wrap correct");
-                        }
-                        score += 100
-                    }else{
-                        for(let j of cardWrap){
-                            j.classList.remove("card_open");
-                        }
-                        score -= 10
-                    }
-                    checkCardNum = 0;
-                    selectedNum = [];
-                    notice.innerText = score;
-                }, 800)
-            }
-        }
-    })
-}
-*/
+document.querySelector("#checkArray").addEventListener("click", e=>{
+    com_createArray();
+})
