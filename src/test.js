d21f2fe8993d99382eed73802bf28c6c7b3185b8
uauu89 <uauu89@gmail.com>
2023-09-13 21:17:38 +0900

    230913 업로드

--- src/test.js
+++ src/test.js
@@ -1,11 +1,20 @@
 let cardArray, openCard, cardArrayCopy, matchCard;
 let userDelay = false;
 
-let com_tunrCount = 0;
+let com_turnCount = 0;
 
-let userSelectedCard = [];
+let selectedCard = [];
 
 let timeDelay = 500;
+
+
+
+/*
+함수 리스트
+
+
+
+*/
 /*
 모든 인덱스값을 가진 배열에서 맞춘 카드의 인덱스 값을 replice로 제거를 하는게 더 낫지 않을까
 > 오픈되었던 카드인지 아닌지 구별 불가능
@@ -24,9 +33,15 @@ openCard 배열에 각 인덱스 위치에 카드번호를 저장
 
 */
 
-document.querySelector("#newGame").addEventListener("click", e=>{
-    init();
-})
+// 테스트용 임시 함수 --------------------------
+function test_printOpenCard(text){
+    document.querySelector("#test_openArray").innerHTML = text;
+}
+function test_printMatchCard(text){
+    document.querySelector("#test_matchArray").innerHTML = text;
+}
+// /테스트용 임시 함수 --------------------------
+
 
 function init(){  
     setCardArray();
@@ -40,69 +55,72 @@ function init(){
 
 }
 
-    function setCardArray(){
-        cardArray = [];
-        let i = 0;
-        while(i < document.querySelector("#inputNum").value){
-            cardArray.push(i+1)
-            i++;
-        }
-
-        cardArrayCopy = [...cardArray, ...cardArray];
-        openCard = "x".repeat(cardArray.length*2).split("");
+function setCardArray(){
+    cardArray = [];
+    let i = 0;
+    while(i < document.querySelector("#inputNum").value){
+        cardArray.push(i+1)
+        i++;
     }
 
-    function cardShuffleSimple(array){
-        array.sort(()=>Math.random() - 0.5);
-    }
+    cardArrayCopy = [...cardArray, ...cardArray];
+    // openCard = "x".repeat(cardArray.length*2).split("");
+    openCard = new Array(cardArray.length*2).fill(null);
+}
 
-    function insertCard(array){
-        let dom_insertHTML = "";
-        array.forEach((i, idx)=>{
-            dom_insertHTML += `
-            <div class="card_wrap" data-value=${i} data-index=${idx}>
-                <div class="card card_front">${i}</div>
-                <div class="card card_back"></div>
-            </div>`;
-        });
-        document.querySelector(".wrap").innerHTML = dom_insertHTML;
-    }
+function cardShuffleSimple(array){
+    array.sort(()=>Math.random() - 0.5);
+}
 
-    function cardCheck(bool){
-        if(bool){
-            document.querySelectorAll(".card_wrap").forEach((card, idx)=>{
-                setTimeout(() => {
-                    card.classList.add("card_open")
-                }, idx*200);
-    
-                setTimeout(() => {
-                    card.classList.remove("card_open")
-                }, idx*100+(cardArray.length*2*200)+3000);
-            })
-            setTimeout(()=>{
-                userDelay = true;
-            }, (cardArray.length*2*300+3000));
-        }else{
+function insertCard(array){
+    let dom_insertHTML = "";
+    array.forEach((i, idx)=>{
+        dom_insertHTML += `
+        <div class="card_wrap" data-value=${i} data-index=${idx}>
+            <div class="card card_front">${i}</div>
+            <div class="card card_back"></div>
+        </div>`;
+    });
+    document.querySelector(".wrap").innerHTML = dom_insertHTML;
+}
+
+function cardCheck(bool){
+    if(bool){
+        document.querySelectorAll(".card_wrap").forEach((card, idx)=>{
+            setTimeout(() => {
+                card.classList.add("card_open")
+            }, idx*200);
+
+            setTimeout(() => {
+                card.classList.remove("card_open")
+            }, idx*100+(cardArray.length*2*200)+3000);
+        })
+        setTimeout(()=>{
             userDelay = true;
-        }
+        }, (cardArray.length*2*300+3000));
+    }else{
+        userDelay = true;
     }
+}
 
 
 
 function pushOpenCard(idx){
 
-    let targetValue = document.querySelectorAll(".card_wrap")[idx].dataset.value;
+    let targetCard = document.querySelectorAll(".card_wrap")[idx];
 
-    openCard[idx] = targetValue;
-    userSelectedCard.push(targetValue);
+    openCard[idx] = targetCard.dataset.value;
+    selectedCard.push(targetCard.dataset.value);
 
-    document.querySelectorAll(".card_wrap")[idx].classList.add("card_open");
-    document.querySelector("#test_openArray").innerHTML = openCard;
+    targetCard.classList.add("card_open");
+    targetCard.classList.add("opend");
+    
+    test_printOpenCard(openCard)
 }
 
 function checkMatching(who){
     let winner = "";
-    if(userSelectedCard[0] === userSelectedCard[1]){
+    if(selectedCard[0] === selectedCard[1]){
         who === "user"? winner = "card_wrap correct user": winner = "card_wrap correct com";
         for(let i of document.querySelectorAll(".card_open")){
             i.setAttribute("class", winner);
@@ -113,7 +131,7 @@ function checkMatching(who){
         }
     }
 
-    userSelectedCard = [];
+    selectedCard = [];
 }
 
 
@@ -130,9 +148,8 @@ function turn_user(){
                 setTimeout(()=>{
                     userDelay = true;
 
-                    if(userSelectedCard.length === 2){
+                    if(selectedCard.length === 2){
                         checkMatching("user");
-
                     }
                 }, timeDelay);
             }
@@ -211,20 +228,53 @@ function com_selectRandom(){
     pushOpenCard(selectedIdx);
 }
 
-function com_selectOpen(){
+function com_selectOpend(){ //필요 없는 함수, 삭제하기
+    let OpenArray = document.querySelectorAll(".opend:not(.card_open)");
+    let selectedIdx = OpenArray[Math.floor(Math.random()*OpenArray.length)].dataset.index;
+    pushOpenCard(selectedIdx);
+}
+
+function com_selectFound(){
+    // openCard
+    // let testArray = [1, 4, null, null, 2, 3, 5, null, 4, null, 1, 3];
+    // testArray.find(e=>{
+    //     console.log(e)
+    // })
+    let removeIdx = document.querySelector(".card_open").dataset.index;
+    let checkArray = [...openCard];
+    checkArray.splice(removeIdx, 1, null);
+
+    test_printMatchCard(checkArray);
+
+    if(checkArray.indexOf(selectedCard[0]) > 0){
+        console.log(checkArray.indexOf(selectedCard[0]));
+        pushOpenCard(checkArray.indexOf(selectedCard[0]));
+    }else{
+        com_selectRandom();
+    }
     
 }
 
 
 function com_turn(){
 
-    if(true){   //나중에 matchCard t/f 조건 추가
+    if(document.querySelectorAll(".opend:not(.card_open)").length && selectedCard.length){
+        com_selectFound();
+        com_turnCount++;
+        document.querySelector("#test_etc").innerHTML = selectedCard;
+    }else{
         com_selectRandom();
-        com_tunrCount++;
-        document.querySelector("#test_etc").innerHTML = userSelectedCard;
+        com_turnCount++;
+        document.querySelector("#test_etc").innerHTML = selectedCard;
     }
-    if(com_tunrCount === 2){
-        com_tunrCount = 0;
+
+    if(!true){   //나중에 matchCard t/f 조건 추가
+        com_selectRandom();
+        com_turnCount++;
+        document.querySelector("#test_etc").innerHTML = selectedCard;
+    }
+    if(com_turnCount === 2){
+        com_turnCount = 0;
         setTimeout(()=>checkMatching("com"), timeDelay);
     }
 }
@@ -233,7 +283,9 @@ function com_turn(){
 /* ----------------------------------------- */
 
 
-
+document.querySelector("#newGame").addEventListener("click", e=>{
+    init();
+})
 
 
 document.querySelector("#aiTest").addEventListener("click", e=>{
@@ -242,3 +294,5 @@ document.querySelector("#aiTest").addEventListener("click", e=>{
 document.querySelector("#checkArray").addEventListener("click", e=>{
     com_createArray();
 })
+
+
